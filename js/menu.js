be3978fb735161b962afb2d567ed7c7b3f23d1ad
krishna <gskrishna44@gmail.com>
2016-05-06 15:06:27 -0500

    Update menu.js
    add pagination and pop-up box

--- js/menu.js
+++ js/menu.js
@@ -4,4 +4,32 @@ $(document).ready(function(){
 		$(this).next().next().toggle( "slow", function(e) {  });
 		$(this).find('span').toggleClass("glyphicon-eye-close");
 	});
+	$(document).on('click', '.pagination form input', function(e){
+		e.preventDefault();
+		getId = $(this).attr('id');
+		$('.page').hide();
+		$('#screen_'+getId).show();
+		$('.pagination form input').attr("class", "");
+		$(this).attr('class', 'active');
+		$( "#dialog-2" ).dialog( "close" );
+	});
+ 
+	$( "#dialog-2" ).dialog({
+		autoOpen: false,
+		maxWidth:400,
+		maxHeight: 300,
+		width: 278,
+		height: 278,
+		modal: true,
+		title: "Menu Selector"
+	});
+
+	$( "#clickableAwesomefont" ).click(function() {
+		$( "#dialog-2" ).dialog( "open" );
+	});
+
+	$( "#save" ).click(function() {
+		$(".alert-success").show().delay(2000).fadeOut();
+	});	
+
 });
